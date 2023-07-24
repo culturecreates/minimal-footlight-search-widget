2f38ef3d852711a00e3562ea71768030526a0c09
syam babu <syam@dexlock.com>
2023-07-24 19:21:54 +0530

    removed i18 backend package

--- src/i18n.js
+++ src/i18n.js
@@ -2,7 +2,6 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import en from "./locales/en/translation.json";
 import fr from "./locales/fr/translation.json";
-import Backend from "i18next-http-backend";
 
 i18n.use(initReactI18next).init({
   debug: true,

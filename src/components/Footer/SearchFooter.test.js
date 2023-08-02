fd11af526336b98059d837233f146f805e5e3f9f
syam babu <syam@dexlock.com>
2023-08-02 18:24:13 +0530

    Unit test for components: Tabs, NoContent, Footer.

--- src/components/Footer/SearchFooter.test.js
+++ src/components/Footer/SearchFooter.test.js
@@ -1,52 +1,60 @@
-import { screen, render, cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom";
 import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import SearchFooter from "./SearchFooter";
-import { I18nextProvider } from "react-i18next";
-import i18n from "../../i18n";
 
-afterEach(() => {
-  cleanup();
+jest.mock("react-i18next", () => ({
+  useTranslation: () => ({
+    t: (key) => key,
+  }),
+}));
+
+describe("SearchFooter", () => {
+  test("renders with the correct message when count > 0", () => {
+    const count = 5;
+    const onSubmit = jest.fn();
+
+    render(<SearchFooter count={count} onSubmit={onSubmit} />);
+
+    const footerElement = screen.getByTestId("footer");
+    expect(footerElement).toBeInTheDocument();
+    expect(footerElement).toHaveStyle("background: var(--primary-flash-yellow, #fff649)");
+    expect(screen.getByText("footer.seeAll")).toBeInTheDocument();
+    expect(screen.getByText(count.toString())).toBeInTheDocument();
+  });
+
+  test("renders with the correct message when count is 0", () => {
+    const count = 0;
+    const onSubmit = jest.fn();
+
+    render(<SearchFooter count={count} onSubmit={onSubmit} />);
+
+    const footerElement = screen.getByTestId("footer");
+    expect(footerElement).toBeInTheDocument();
+    expect(footerElement).toHaveStyle("background: var(--primary-light-yellow, #F2EB9F)");
+    expect(screen.getByText("footer.noItems")).toBeInTheDocument();
+    expect(screen.getByText(count.toString())).toBeInTheDocument();
+  });
+
+  test("does not call onSubmit function when count is 0 and clicked", () => {
+    const count = 0;
+    const onSubmit = jest.fn();
+
+    render(<SearchFooter count={count} onSubmit={onSubmit} />);
+    const footerElement = screen.getByTestId("footer");
+    fireEvent.click(footerElement);
+
+    expect(onSubmit).not.toHaveBeenCalled();
+  });
+
+  test("calls onSubmit function when count > 0 and clicked", () => {
+    const count = 5;
+    const onSubmit = jest.fn();
+
+    render(<SearchFooter count={count} onSubmit={onSubmit} />);
+    const footerElement = screen.getByTestId("footer");
+    fireEvent.click(footerElement);
+
+    expect(onSubmit).toHaveBeenCalled();
+  });
 });
-
-test("should render 'seeAll' message when count > 0", () => {
-  const count = 5;
-  const onSubmitMock = jest.fn();
-
-  // Render the component with the mocked props
-  const footer = render(
-    <I18nextProvider i18n={i18n}>
-      <SearchFooter count={count} onSubmit={onSubmitMock} />
-    </I18nextProvider>
-  );
-
-  //   const seeAllMessage = footer
-  //     .getByText(i18n.getDataByLanguage("en").translation.footer.seeAll)
-  //     .toBeDefined();
-  //   expect(seeAllMessage).toBeInTheDocument();
-
-  const noItemsMessage = footer.queryByText(
-    i18n.getDataByLanguage("en").translation.footer.noItems
-  );
-console.log(noItemsMessage,"NO ITEMS MESSAGE");
-  expect(noItemsMessage).not.toBeInTheDocument();
-});
-
-// test("should render 'noItems' message when count <= 0", () => {
-//   // Mock the count and onSubmit prop values
-//   const count = 0;
-//   const onSubmitMock = jest.fn();
-
-//   // Render the component with the mocked props
-//   const footer = render(
-//     <I18nextProvider i18n={i18n}>
-//       <SearchFooter count={count} onSubmit={onSubmitMock} />
-//     </I18nextProvider>
-//   );
-
-//   // Assert that the 'noItems' message is rendered in the component
-//   const noItemsMessage = footer
-//     .getByText(i18n.getDataByLanguage("en").translation.footer.noItems)
-//     .toBeDefined();
-//   expect(noItemsMessage).toBeInTheDocument();
-// });
